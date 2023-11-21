9b7204747baec4c858da1e021a560a0b33231c15
TONGQIA <1227894812@qq.com>
2023-11-21 18:42:41 +0800

    init

---
+++ src/main/java/com/tong/flyojcodesandbox/FlyojCodeSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.tong.flyojcodesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyojCodeSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlyojCodeSandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tong/flyojcodesandbox/FlyojCodeSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tong.flyojcodesandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyojCodeSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
